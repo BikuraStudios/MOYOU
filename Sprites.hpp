c7492cb6e62c4f1cb6b8d31c6f4d05b2f8b457c9
ルーカス　ビークラ <bikurastudios@gmail.com>
2025-07-04 23:38:14 +0900

    The title screen and main window for Moyou. Sprites atlas is fully implemented as well as backgrounds and basic buttons. No sound or actual gameplay implemented as of yet.

---
+++ Sprites.hpp
@@ -0,0 +1,118 @@
+//=================================================
+//--------------------TEXTURES--------------------
+//=================================================
+sf::Texture texture_atlas("atlas.png");
+sf::Texture texture_title("title.png");
+sf::Texture texture_gameBoard("gameBoard.png");
+sf::Texture texture_checkBack("checkBack.png");
+sf::Texture texture_clouds("clouds.png");
+sf::Texture texture_cloudsTwo("cloudsTwo.png");
+sf::Texture texture_logo("logo.png");
+
+
+
+
+//=================================================
+//  --------------------RECTS--------------------
+//=================================================
+
+//--------------------tiles--120x120px--------------------
+
+
+inline const sf::IntRect purpleTileRect({ 0, 0 }, { 120, 120 });
+inline const sf::IntRect brownTileRect({ 120, 0 }, { 120, 120 });
+inline const sf::IntRect tealTileRect({ 240, 0 }, { 120, 120 });
+inline const sf::IntRect orangeTileRect({ 360, 0 }, { 120, 120 });
+inline const sf::IntRect greyTileRect({ 480, 0 }, { 120, 120 });
+inline const sf::IntRect mintTileRect({ 600, 0 }, { 120, 120 });
+inline const sf::IntRect yellowTileRect({ 720, 0 }, { 120, 120 });
+inline const sf::IntRect blackTileRect({ 840, 0 }, { 120, 120 });
+inline const sf::IntRect greenTileRect({ 960, 0 }, { 120, 120 });
+inline const sf::IntRect whiteTileRect({ 1080, 0 }, { 120, 120 });
+inline const sf::IntRect BG1TileRect({ 0, 0 }, { 120, 120 });
+inline const sf::IntRect BG2TileRect({ 120, 0 }, { 120, 120 });
+inline const sf::IntRect BG3TileRect({ 240, 0 }, { 120, 120 });
+inline const sf::IntRect BG4TileRect({ 360, 0 }, { 120, 120 });
+inline const sf::IntRect swapTileRect({ 480, 0 }, { 120, 120 });
+inline const sf::IntRect targetTileRect({ 600, 0 }, { 120, 120 });
+inline const sf::IntRect selectTileRect({ 720, 0 }, { 120, 120 });
+inline const sf::IntRect vertTileRect({ 840, 0 }, { 120, 120 });
+inline const sf::IntRect horzTileRect({ 960, 0 }, { 120, 120 });
+inline const sf::IntRect crossTileRect({ 1080, 0 }, { 120, 120 });
+
+//--------------------controller-mockup--200x200px--------------------
+inline const sf::IntRect controllerRect({ 0, 240 }, { 200, 200 });
+inline const sf::IntRect confirmRect({ 200, 240 }, { 200, 200 });
+inline const sf::IntRect cancelRect({ 400, 240 }, { 200, 200 });
+
+//--------------------start-menu-buttons--339x103px--------------------
+inline const sf::IntRect startRect({ 861, 265 }, { 339, 103 });
+inline const sf::IntRect quitRect({ 861, 377 }, { 339, 103 });
+
+//----------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+//=================================================
+// --------------------SPRITES--------------------
+//=================================================
+
+extern sf::Texture texture_atlas;
+
+//----------------tiles------------------------------------
+inline sf::Sprite purpleTile(texture_atlas, purpleTileRect);
+inline sf::Sprite brownTile(texture_atlas, brownTileRect);
+inline sf::Sprite tealTile(texture_atlas, tealTileRect);
+inline sf::Sprite orangeTile(texture_atlas, orangeTileRect);
+inline sf::Sprite greyTile(texture_atlas, greyTileRect);
+inline sf::Sprite mintTile(texture_atlas, mintTileRect);
+inline sf::Sprite yellowTile(texture_atlas, yellowTileRect);
+inline sf::Sprite blackTile(texture_atlas, blackTileRect);
+inline sf::Sprite greenTile(texture_atlas, greenTileRect);
+inline sf::Sprite whiteTile(texture_atlas, whiteTileRect);
+inline sf::Sprite BG1Tile(texture_atlas, BG1TileRect);
+inline sf::Sprite BG2Tile(texture_atlas, BG2TileRect);
+inline sf::Sprite BG3Tile(texture_atlas, BG3TileRect);
+inline sf::Sprite BG4Tile(texture_atlas, BG4TileRect);
+inline sf::Sprite swapTile(texture_atlas, swapTileRect);
+inline sf::Sprite targetTile(texture_atlas, targetTileRect);
+inline sf::Sprite selectTile(texture_atlas, selectTileRect);
+inline sf::Sprite vertTile(texture_atlas, vertTileRect);
+inline sf::Sprite horzTile(texture_atlas, horzTileRect);
+inline sf::Sprite crossTile(texture_atlas, crossTileRect);
+
+//---------------controller--------------------------------
+inline sf::Sprite controllerSprite(texture_atlas, controllerRect);
+inline sf::Sprite confirmSprite(texture_atlas, confirmRect);
+inline sf::Sprite cancelSprite(texture_atlas, cancelRect);
+
+//--------------title-menu-buttons-------------------------
+inline sf::Sprite startHighlight(texture_atlas, startRect);
+inline sf::Sprite quitHighlight(texture_atlas, quitRect);
+
+//-------------backgrounds---------------------------------
+inline sf::Sprite titleScreen(texture_title);
+inline sf::Sprite gameBoard(texture_gameBoard);
+inline sf::Sprite clouds(texture_clouds);
+inline sf::Sprite cloudsTwo(texture_cloudsTwo);
+inline sf::Sprite checkBackground(texture_checkBack);
+
+//--------------------misc-----------------------------------
+inline sf::Sprite logo(texture_logo);
+
+
+//=================================================
+//  --------------------FONTS--------------------
+//=================================================
+sf::Font font("PressStart2P.ttf");
+
+
+//=================================================
+//  --------------------AUDIO--------------------
+//=================================================
---
+++ main.cpp
@@ -0,0 +1,181 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include "Sprites.hpp"
+
+
+
+//=============================================
+//--------------STRUCTS------------------------
+//=============================================
+
+struct ParallaxLayer
+{
+    sf::Sprite sprite;
+    float offset = 0.0f;
+    float speed = 0.0f;
+    float resetThreshhold = 1922.0f;
+
+    ParallaxLayer(const sf::Texture& texture, float speed, float resetThreshhold = 1922.0f)
+        : sprite(texture), speed(speed), resetThreshhold(resetThreshhold)
+    {
+    }
+
+    void update(float deltaTime)
+    {
+        if (offset > -resetThreshhold)
+            offset -= speed * deltaTime;
+        if (offset <= -resetThreshhold)
+            offset = 0.0f;
+
+        sprite.setPosition(sf::Vector2(offset, 0.0f));
+    }
+    void draw(sf::RenderWindow& window)
+    {
+        window.draw(sprite);
+    }
+};
+
+//===========================================================================
+//------------------------STATES---------------------------------------------
+//===========================================================================
+bool startUpState{ 0 };
+bool startMenuState{ 1 };
+bool mainGameState{ 0 };
+
+
+
+//===========================================================================
+//------------------------VARIABLES------------------------------------------
+//===========================================================================
+
+ParallaxLayer parallaxClouds(texture_clouds, 30.0f);
+ParallaxLayer parallaxCloudsTwo(texture_cloudsTwo, 75.0f);
+ParallaxLayer parallaxBackCheck(texture_checkBack, 125.0f);
+
+bool startQuit{ 0 };
+
+
+//===========================================================================
+//------------------------FUNCIONS-------------------------------------------
+//===========================================================================
+
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({ 1915u, 1075u }), "MOYOU");
+    window.setFramerateLimit(144);
+    window.setPosition({ -8,0 });
+    sf::View gameView(sf::FloatRect({ 0.0f, 0.0f }, { 1925.f, 1085.f }));
+//---------------------------------------------------------------------------
+    sf::Clock clock;
+ //---------------------------------------------------------------------------
+
+
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+               
+//--------------------------Start-Logo------------------------------------------------
+
+
+//-------------------------/Start-Logo------------------------------------------------
+//====================================================================================
+//--------------------------Start-Menu------------------------------------------------                
+                if (startMenuState){
+                if ((keyPressed->scancode == sf::Keyboard::Scancode::Up) || (keyPressed->scancode == sf::Keyboard::Scancode::Down)){
+                    startQuit = !startQuit;
+                    std::cout << startQuit << "\n";
+                }
+                if ((keyPressed->scancode == sf::Keyboard::Scancode::Enter)) {
+                    if (!startQuit) {
+                        mainGameState = 1;
+                        startMenuState = 0;
+                    }
+                    else {
+                        window.close();
+                    }
+                }
+                }
+//---------------------------/Start-Menu-----------------------------------------------
+//====================================================================================
+//---------------------------Main-Game-----------------------------------------------
+// 
+//--------------------------/Main-Game-----------------------------------------------
+
+                
+            
+            }
+            if (event->is<sf::Event::Resized>())
+            {
+                sf::FloatRect viewport;
+                sf::Vector2u size = window.getSize();
+                auto [windowWidth, windowHeight] = size;
+                float aspectRatio = windowWidth / windowHeight;
+                float targetRatio{ 16.f / 9.f };
+
+
+                if (aspectRatio > targetRatio)
+                {
+                    // Window is too wide — horizontal letterboxing
+                    float width = targetRatio / aspectRatio;
+                    viewport = sf::FloatRect({ ((1.f - width) / 2.f), 0.f }, { width, 1.f });
+                }
+                else
+                {
+                    // Window is too tall — vertical letterboxing
+                    float height = aspectRatio / targetRatio;
+                    viewport = sf::FloatRect({ 0.f, ((1.f - height) / 2.f) }, { 1.f, height });
+                }
+
+                gameView.setViewport(viewport);
+            }
+//---------------------------------------------------------------------------------------------------------------
+
+            
+//---------------------------------------------------------------------------------------------------------------
+        }
+        float deltaTime = clock.restart().asSeconds();
+        parallaxClouds.update(deltaTime);
+        parallaxBackCheck.update(deltaTime);
+        parallaxCloudsTwo.update(deltaTime);
+
+        window.setView(gameView);
+        window.clear();
+
+        //--------------------STARTUP------------------------------------------------------------------------------
+        
+        if (startMenuState) {
+        parallaxBackCheck.draw(window);
+        window.draw(titleScreen);
+        startHighlight.setPosition({ 790.f,590.f });
+        quitHighlight.setPosition({ 790.f,765.f });
+
+        if (!startQuit) {            
+            window.draw(startHighlight);
+        }
+        else {
+            window.draw(quitHighlight);
+        }
+        }
+
+        //-----------------Main-Game---------------------------------------------------------------------------------
+        if (mainGameState){
+        parallaxClouds.draw(window);
+        parallaxCloudsTwo.draw(window);
+        window.draw(gameBoard);
+        }
+
+        window.display();
+    }
+}
